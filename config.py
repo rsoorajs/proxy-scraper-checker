b8c124f2f0fe7e4c6b9e2685c8eaca18f1f11d0a
monosans <hsyqixco@protonmail.com>
2021-10-03 10:48:40 +0300

    Fix default sources

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ HTTP_SOURCES = {
     "https://raw.githubusercontent.com/chipsed/proxies/main/proxies.txt",
     "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
     "https://raw.githubusercontent.com/hendrikbgr/Free-Proxy-Repo/master/proxy_list.txt",
-    "https://raw.githubusercontent.com/jetkai/free-proxies-autoupdated/main/proxies-http%2Bhttps.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http%2Bhttps.txt",
     "https://raw.githubusercontent.com/KUTlime/ProxyList/main/ProxyList.txt",
     "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
     "https://raw.githubusercontent.com/mmpx12/proxy-list/master/https.txt",
@@ -30,7 +30,7 @@ HTTP_SOURCES = {
 SOCKS4 = True
 SOCKS4_SOURCES = {
     "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4",
-    "https://raw.githubusercontent.com/jetkai/free-proxies-autoupdated/main/proxies-socks4.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt",
     "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt",
     "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt",
     "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
@@ -42,7 +42,7 @@ SOCKS5 = True
 SOCKS5_SOURCES = {
     "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5",
     "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
-    "https://raw.githubusercontent.com/jetkai/free-proxies-autoupdated/main/proxies-socks5.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt",
     "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt",
     "https://raw.githubusercontent.com/roma8ok/proxy-list/main/proxy-list-socks5.txt",
     "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt",
